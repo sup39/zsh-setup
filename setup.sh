a16171cba821bc1010a9c19c41802055a9fd2196
sup39 <sup39hakujun@gmail.com>
2020-06-14 18:40:32 +0900

    fix vim comment

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ curl https://raw.githubusercontent.com/sup39/zsh-setup/master/zshrc -o $HOME/.zs
 [ -d $HOME/.vim/sup39 ] || git clone https://github.com/sup39/vimrc $HOME/.vim/sup39
 # .vimrc
 [ -f $HOME/.vimrc ] || cat >$HOME/.vimrc <<EOF
-# source $HOME/.vim/sup39/plug.vim
+" source $HOME/.vim/sup39/plug.vim
 source $HOME/.vim/sup39/vimrc
 EOF
